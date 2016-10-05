263187fa730d8efa489ff3565d2f2172b6d06c4c
Rupert Smith <rupertlssmith@github.com>
2016-10-05 16:57:34 +0100

    Using Html.Keyed, I have no clue...

--- src/elm/MultiSelect/View.elm
+++ src/elm/MultiSelect/View.elm
@@ -2,6 +2,7 @@ module Multiselect.View exposing (root)
 
 import Html exposing (..)
 import Html.Attributes exposing (title, class, type', attribute)
+import Html.Keyed
 import Material.Button as Button
 import Material.Icon as Icon
 import Multiselect.Types exposing (..)
@@ -13,20 +14,20 @@ root model =
         [ div [ class "horizontal-section-container" ]
             [ h4 [] [ text "Multiselect" ]
             , paperListBox [ attribute "multi" "" ]
-                [ paperItem [] [ text "Bold" ]
-                , paperItem [] [ text "Italic" ]
-                , paperItem [] [ text "Underline" ]
-                , paperItem [] [ text "Strikethrough" ]
+                [ ( "item1", paperItem [] [ ( "1", text "Bold" ) ] )
+                , ( "item2", paperItem [] [ ( "2", text "Italic" ) ] )
+                , ( "item3", paperItem [] [ ( "3", text "Underline" ) ] )
+                , ( "item4", paperItem [] [ ( "4", text "Strikethrough" ) ] )
                 ]
             ]
         ]
 
 
-paperListBox : List (Attribute a) -> List (Html a) -> Html a
+paperListBox : List (Attribute a) -> List ( String, Html a ) -> Html a
 paperListBox =
-    Html.node "paper-listbox"
+    Html.Keyed.node "paper-listbox"
 
 
-paperItem : List (Attribute a) -> List (Html a) -> Html a
+paperItem : List (Attribute a) -> List ( String, Html a ) -> Html a
 paperItem =
-    Html.node "paper-item"
+    Html.Keyed.node "paper-item"
